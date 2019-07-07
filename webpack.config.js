13d4ba4399866dbb05308ecc124c48e8e2173180
Sota Makino <sm96kyoto@gmail.com>
2019-07-07 23:41:21 +0900

    start

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,39 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path');
+const dist = path.join(__dirname, 'dist')
 
 module.exports = {
-    entry: './src/index.tsx',
-    output: {
-        path: __dirname + '/dist',
-        filename: 'index_bundle.js'
-    },
-    mode: "production",
-
-    resolve: {
-        // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: [".ts", ".tsx"]
-    },
-
-    module: {
-        rules: [{
-                test: /\.ts(x?)$/,
-                exclude: /node_modules/,
-                use: [{
-                    loader: "ts-loader"
-                }]
-            },
-            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            {
-                enforce: "pre",
-                test: /\.js$/
-            }
-        ]
-    },
-
-    // When importing a module whose path matches one of the following, just
-    // assume a corresponding global variable exists and use that instead.
-    // This is important because it allows us to avoid bundling all of our
-    // dependencies, which allows browsers to cache those libraries between builds.
-    externals: {
-        "react": "React",
-        "react-dom": "ReactDOM"
-    },
-
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/index.html",
-            filename: "./index.html"
-        })
+  entry: './src/index.tsx',
+  output: {
+    path: dist,
+    filename: 'index_bundle.js'
+  },
+  resolve: {
+    modules: ['node_modules'],
+    extensions: [".ts", ".tsx", '.js', '.jsx']
+  },
+  module: {
+    rules: [{
+        test: /\.ts(x?)$/,
+        exclude: /node_modules/,
+        use: [{
+          loader: "ts-loader"
+        }]
+      },
+      {
+        enforce: "pre",
+        test: /\.js$/
+      }
     ]
+  },
+  externals: {
+    "react": "React",
+    "react-dom": "ReactDOM"
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ]
 };
